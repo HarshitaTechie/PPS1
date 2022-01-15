375842c5b800114a9653942a3b6a10f6863f7470
HarshTechie <97089353+HarshitaTechie@users.noreply.github.com>
2022-01-15 12:27:44 +0000

    Practice Program 2

--- p1original.c
+++ p1original.c
@@ -1,2 +1,13 @@
-
+#include<stdio.h>
+int main(void)
+{
+  int a,b,tot;
+  printf("Please enter two numbers you want to add \n");
+  printf("Number 1: ");
+  scanf("%d",&a);
+  printf("Number 2: ");
+  scanf("%d",&b);
+  tot=a+b;
+  printf("The sum of %d and %d is %d .\n",a,b,tot);
+  return 0;
 }
